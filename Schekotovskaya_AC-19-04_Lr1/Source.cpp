f643277a1c9a8d7de7a70b1087c0e5673339abc0
SchekotovskayaMariya <70648159+SchekotovskayaMariya@users.noreply.github.com>
2020-10-11 20:16:00 +0300

    structs

--- Schekotovskaya_AC-19-04_Lr1/Source.cpp
+++ Schekotovskaya_AC-19-04_Lr1/Source.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
-
+//#include <string>
 using namespace std;
 
-int main()
+struct pipe
 {
-    int k;
-    cin >> k;
-    cout << "Hello World!\n";
-    return 0;
-}
+	int id;
+	float length, diameter;
+	bool repair_status;
+};
+
+struct KC
+{
+	int id, n_ws, n_ws_op; // n_ws - number of workshops, n_ws_op - number of workshops in operation
+	string name;
+	float ef; // ef - efficiency
+};
