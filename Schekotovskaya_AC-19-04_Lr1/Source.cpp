2f1f7863595ec1847a72f43604ab01b31e5ca5f6
SchekotovskayaMariya <70648159+SchekotovskayaMariya@users.noreply.github.com>
2020-10-26 18:32:54 +0300

    editing the menu

--- Schekotovskaya_AC-19-04_Lr1/Source.cpp
+++ Schekotovskaya_AC-19-04_Lr1/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -17,68 +18,216 @@ struct KC
 	float ef; // ef - efficiency
 };
 
-pipe createpipe()
+pipe CreatePipe()
 {
 	pipe p;
 
 	//cout << "Type the pipe ID: ";
 	p.id = 0;
 
-	cout << "Type the length of the pipe: ";
+	cout << "\nType the length of the pipe: ";
 	cin >> p.length;
 
-	cout << "Type the diameter of the pipe: ";
+	cout << "\nType the diameter of the pipe: ";
 	cin >> p.diameter;
 
-	cout << "Type the status of the repeir pipe: ";
+	cout << "\nType the status of the repair pipe: ";
 	cin >> p.repair_status;
+	
+	return p;
 }
 
-KC createKC()
+void ViewPipe(const pipe& p)
+{
+	cout << "\nID: " << p.id << endl
+		<< "Length: " << p.length << endl
+		<< "Diametr: " << p.diameter << endl
+		<< "Repair status: " << p.repair_status << endl;
+}
+
+void SavePipe(const pipe& p)
+{
+	ofstream fout;
+	fout.open("data1.txt", 'w');
+
+	fout << "\nID: " << p.id << endl
+		<< "Length: " << p.length << endl
+		<< "Diametr: " << p.diameter << endl
+		<< "Repair status: " << p.repair_status << endl;
+}
+
+void EditPipe(pipe& p)
+{
+	if (p.repair_status == 1)
+	{
+		p.repair_status = 0;
+		cout << "\nStatus changed to pipe not under repair\n";
+	}
+	else
+	{
+		p.repair_status = 1;
+		cout << "\nStatus changed to pipe under repair\n";
+	}
+
+}
+
+KC CreateKC()
 {
 	KC s;
 
-	//cout << "Type the KC ID: " << endl;
+	//cout << "Type the KC ID: ";
 	s.id = 0;
 
-	cout << "Type a name of the KC: ";
+	cout << "\nType a name of the KC: ";
 	cin >> s.name;
 
-	cout << "Type the number of workshops: ";
+	cout << "\nType the number of workshops: ";
 	cin >> s.n_ws;
 
-	cout << "Type the number of workshops in operation: ";
+	cout << "\nType the number of workshops in operation: ";
 	cin >> s.n_ws_op;
 
-	cout << "Type the efficiency of the KC: ";
+	cout << "\nType the efficiency of the KC: ";
 	cin >> s.ef;
+
+	return s;
 }
 
-void menu()
+void ViewKC(const KC& s)
 {
-	cout << "Main menu" << endl << "1. Add a pipe" << endl << "2. Add a KC" << endl << "3. View all objects" << endl << "4. Edit the pipe" << endl << "5. Edit the KC" << endl << "6. Save" << endl << "7. Upload" << endl << "0. Exit" << endl<< "Select the desired number from the menu: ";
+	cout << "\nID: " << s.id << endl
+		<< "Name: " << s.name << endl
+		<< "Number of workshops: " << s.n_ws << endl
+		<< "Number of workshops in operation: " << s.n_ws_op << endl
+		<< "Efficiency: " << s.ef << endl;
+}
 
+void SaveKC(const KC& s)
+{
+	ofstream fout;
+	fout.open("data2.txt", 'w');
+
+	fout << "\nID: " << s.id << endl
+		<< "Name: " << s.name << endl
+		<< "Number of workshops: " << s.n_ws << endl
+		<< "Number of workshops in operation: " << s.n_ws_op << endl
+		<< "Efficiency: " << s.ef << endl;
+}
+
+void EditKC(KC& s)
+{
+
+	cout << "\n1. Turn on the workshop ";
+	cout << "\n2. Turn off the workshop";
+	cout << "\nChoose: ";
+	int choiñe;
+	cin >> choiñe;
+	if (choiñe == 1)
+	{
+		s.n_ws_op += 1;
+	}
+	else
+	{
+		s.n_ws_op -= 1;
+	}
+}
+
+void Menu()
+{
+	cout << "\nMain menu\n" << "1. Add a pipe\n" << "2. Add a KC\n" << "3. View all objects\n" << "4. Edit the pipe\n" << "5. Edit the KC\n"<< "6. Save\n" << "7. Upload\n" << "0. Exit\n" << "Select the desired number from the menu: ";
+
+}
+
+void SaveAll(const pipe& p, const KC& s)
+{
+	ofstream fout;
+	fout.open("data3.txt", 'w');
+
+
+	fout << "\nID: " << p.id << endl
+		<< "Length: " << p.length << endl
+		<< "Diametr: " << p.diameter << endl
+		<< "Repair status: " << p.repair_status << endl;
+
+	fout << "\nID: " << s.id << endl
+		<< "Name: " << s.name << endl
+		<< "Number of workshops: " << s.n_ws << endl
+		<< "Number of workshops in operation: " << s.n_ws_op << endl
+		<< "Efficiency: " << s.ef << endl;
+
+}
+
+void ViewThat(int m,const pipe& pi,const KC& st)
+{
+	switch (m)
+	{
+	case 1:
+		ViewPipe(pi);
+		break;
+	case 2:
+		ViewKC(st);
+		break;
+	}
+}
+void SaveThat(int l,const pipe& pi,const KC& st)
+{
+	switch (l)
+	{
+	case 1:
+		SavePipe(pi);
+		break;
+	case 2:
+		SaveKC(st);
+		break;
+	case 3:
+		SaveAll(pi, st);
+		break;
+	}
 }
 
 int main()
-{
-	menu();
-	int n;
-	cin >> n;
+{ 
+	pipe pi;
+	KC st; // óçíàòü ïî÷åìó íå ðàáîòàåò â ñâè÷å
 	
-	switch (n)
+
+	while (true)
 	{
-	case 1:
-		createpipe();
-		break;
-	case 2:
-		createKC();
-		break;
-	case 3 :
-	case 4 :
-	case 5 :
-	case 6 :
-	case 7 :
-	case 8 :
-	}
+		Menu();
+		int n;
+		cin >> n;
+
+		switch (n)
+		{
+		case 1:
+			pi = CreatePipe();
+			break;
+		case 2:
+			st = CreateKC();
+			break;
+		case 3:
+			cout << "Select object 1-Pipe 2-KC: ";
+			int m;
+			cin >> m;
+			ViewThat(m, pi, st);
+			break;
+		case 4:
+			EditPipe(pi);
+			break;
+		case 5:
+			EditKC(st);
+			break;
+		case 6 :
+			cout << "Select object 1-Pipe 2-KC 3-All: ";
+			int l;
+			cin >> l;
+			SaveThat(l, pi, st);
+			break;
+			//case 7 :
+		case 0 :
+			return 0;
+		}
+	} 
+	return 0;
+	
 }
